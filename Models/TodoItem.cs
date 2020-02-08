cecd1364d5ed9af8c5a61013810bddf6e8e752b6
unknown <mah84m@gmail.com>
2020-02-08 15:57:17 +0300

    Changes include desription tag

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -10,5 +10,6 @@ namespace Todo.Models
         public long id { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
+        public string Description { get; set; }
     }
 }
